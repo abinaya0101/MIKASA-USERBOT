b9fab8c0030ed5dc85f14615be5156848d0d648e
DEADLY-FIGHTERS <DEADLY-FIGHTERS@users.noreply.github.com>
2021-08-08 14:43:47 +0000

    Replaced new name

--- ex_config.py
+++ ex_config.py
@@ -8,7 +8,7 @@ class Development(Config):
 
   # the name to display in your deadly message.
   # If not filled anything then default value is I'm Hêll.
-  YOUR_NAME = "꧁༺ʟɛɢɛռɖǟʀʏ ǟʄ Dєα∂ly༻꧂"
+  YOUR_NAME = "꧁༺ʟɛɢɛռɖǟʀʏ ǟʄ ɖɛǟɖʟʏ ɮօȶ༻꧂"
 
   # create any PostgreSQL database.
   # I recommend to use elephantsql and paste that link here
